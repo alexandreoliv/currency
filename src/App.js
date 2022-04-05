897d48fae26b879fcea7e84a691a4e8b176aea25
alexandreoliv <alexandrebo@gmail.com>
2022-04-05 21:55:45 +0200

    add basic Ant layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Layout, Menu, Breadcrumb } from "antd";
+import { Dropdown, Button, message, Space } from "antd";
+import { DownOutlined, UserOutlined } from "@ant-design/icons";
+const { Header, Content, Footer } = Layout;
+
+function handleMenuClick(e) {
+	message.info("Click on menu item.");
+	console.log("click", e);
+}
+
+const menu = (
+	<Menu onClick={handleMenuClick}>
+		<Menu.Item key="1" icon={<UserOutlined />}>
+			1st menu item
+		</Menu.Item>
+		<Menu.Item key="2" icon={<UserOutlined />}>
+			2nd menu item
+		</Menu.Item>
+	</Menu>
+);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Layout>
+			<Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+				<div className="logo" />
+				<Menu
+					theme="dark"
+					mode="horizontal"
+					defaultSelectedKeys={["2"]}
+				>
+					<Menu.Item key="1">nav 1</Menu.Item>
+					<Menu.Item key="2">nav 2</Menu.Item>
+				</Menu>
+			</Header>
+			<Content
+				className="site-layout"
+				style={{ padding: "0 50px", marginTop: 64 }}
+			>
+				<Breadcrumb style={{ margin: "16px 0" }}>
+					<Breadcrumb.Item>Home</Breadcrumb.Item>
+					<Breadcrumb.Item>List</Breadcrumb.Item>
+					<Breadcrumb.Item>App</Breadcrumb.Item>
+				</Breadcrumb>
+				<div
+					className="site-layout-background"
+					style={{ padding: 24, minHeight: 380 }}
+				>
+					<Space wrap>
+						<Dropdown overlay={menu}>
+							<Button>
+								Button <DownOutlined />
+							</Button>
+						</Dropdown>
+					</Space>
+				</div>
+			</Content>
+			<Footer style={{ textAlign: "center" }}>
+				Ant Design ©2018 Created by Ant UED
+			</Footer>
+		</Layout>
+	);
 }
 
 export default App;
