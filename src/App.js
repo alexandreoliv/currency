8206c75201f831aeac7fdc38e9ccd9a7983b3288
alexandreoliv <alexandrebo@gmail.com>
2022-04-05 22:44:54 +0200

    add axios call to API

--- src/App.js
+++ src/App.js
@@ -2,44 +2,65 @@ import "./App.css";
 import { Layout, Menu, Breadcrumb } from "antd";
 import SelectCurrency from "./Components/SelectCurrency";
 import Rates from "./Components/Rates";
+import { Component } from "react";
 const { Header, Content, Footer } = Layout;
+const axios = require("axios");
 
-function App() {
-	return (
-		<Layout>
-			<Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
-				<div className="logo" />
-				<Menu
-					theme="dark"
-					mode="horizontal"
-					defaultSelectedKeys={["1"]}
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { rates: "" };
+	}
+
+	// componentDidMount() {
+	// 	axios
+	// 		.get(
+	// 			`http://api.exchangeratesapi.io/v1/latest?access_key=${process.env.REACT_APP_SECRET_KEY}`
+	// 		)
+	// 		.then((response) => {
+	// 			console.log(response.data.rates);
+	// 			this.setState({ rates: response.data.rates });
+	// 		})
+	// 		.catch((err) => console.log(err));
+	// }
+
+	render() {
+		return (
+			<Layout>
+				<Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+					<div className="logo" />
+					<Menu
+						theme="dark"
+						mode="horizontal"
+						defaultSelectedKeys={["1"]}
+					>
+						<Menu.Item key="1">nav 1</Menu.Item>
+						<Menu.Item key="2">nav 2</Menu.Item>
+					</Menu>
+				</Header>
+				<Content
+					className="site-layout"
+					style={{ padding: "0 50px", marginTop: 64 }}
 				>
-					<Menu.Item key="1">nav 1</Menu.Item>
-					<Menu.Item key="2">nav 2</Menu.Item>
-				</Menu>
-			</Header>
-			<Content
-				className="site-layout"
-				style={{ padding: "0 50px", marginTop: 64 }}
-			>
-				<Breadcrumb style={{ margin: "16px 0" }}>
-					<Breadcrumb.Item>Home</Breadcrumb.Item>
-					<Breadcrumb.Item>List</Breadcrumb.Item>
-					<Breadcrumb.Item>App</Breadcrumb.Item>
-				</Breadcrumb>
-				<div
-					className="site-layout-background"
-					style={{ padding: 24, minHeight: 380 }}
-				>
-					<SelectCurrency />
-					<Rates />
-				</div>
-			</Content>
-			<Footer style={{ textAlign: "center" }}>
-				Ant Design ©2018 Created by Ant UED
-			</Footer>
-		</Layout>
-	);
+					<Breadcrumb style={{ margin: "16px 0" }}>
+						<Breadcrumb.Item>Home</Breadcrumb.Item>
+						<Breadcrumb.Item>List</Breadcrumb.Item>
+						<Breadcrumb.Item>App</Breadcrumb.Item>
+					</Breadcrumb>
+					<div
+						className="site-layout-background"
+						style={{ padding: 24, minHeight: 380 }}
+					>
+						<SelectCurrency />
+						<Rates />
+					</div>
+				</Content>
+				<Footer style={{ textAlign: "center" }}>
+					Ant Design ©2018 Created by Ant UED
+				</Footer>
+			</Layout>
+		);
+	}
 }
 
 export default App;
